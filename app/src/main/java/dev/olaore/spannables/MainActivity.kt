626f4bf9c7fed6dbd529baa28a25496fc7bd5e33
Foodie <olaorefouad565@gmail.com>
2020-04-25 10:34:57 +0100

    feature: looked into more span types like foreground spans, background, absolute and relative size spans and also strikethrough span - research still goes on!

--- app/src/main/java/dev/olaore/spannables/MainActivity.kt
+++ app/src/main/java/dev/olaore/spannables/MainActivity.kt
@@ -1,19 +1,21 @@
 package dev.olaore.spannables
 
 import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.Spanned
-import android.text.style.AbsoluteSizeSpan
-import android.text.style.ForegroundColorSpan
-import android.text.style.URLSpan
+import android.text.style.*
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,23 +34,35 @@ class MainActivity : AppCompatActivity() {
                 "End Of PP: ${ termsOfServiceText.indexOf(privacyPolicy) + privacyPolicy.length
         }")
 
-        spannable.append(termsOfServiceText)
+        spannable.append(Html.fromHtml("Find more details in <br/> $privacyPolicy and $termsOfService", Html.FROM_HTML_MODE_LEGACY))
+        spannable.setSpan(
+            RelativeSizeSpan(1.5f),
+            termsOfServiceText.indexOf("Find"),
+            termsOfServiceText.indexOf("Find") + "Find".length,
+            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(
+            ForegroundColorSpan(Color.parseColor("#FFA500")),
+            termsOfServiceText.indexOf(privacyPolicy),
+            termsOfServiceText.indexOf(privacyPolicy) + privacyPolicy.length,
+            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(
+            StrikethroughSpan(),
+            termsOfServiceText.indexOf("details"),
+            termsOfServiceText.indexOf("details") + "details".length,
+            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(
+            BackgroundColorSpan(Color.RED),
+            termsOfServiceText.indexOf(privacyPolicy),
+            termsOfServiceText.indexOf(privacyPolicy) + privacyPolicy.length,
+            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
+        )
         spannable.setSpan(
             ForegroundColorSpan(Color.RED),
-            termsOfServiceText.indexOf(privacyPolicy),
-            termsOfServiceText.indexOf(privacyPolicy) + privacyPolicy.length,
-            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
-        )
-        spannable.setSpan(
-            AbsoluteSizeSpan(30),
-            termsOfServiceText.indexOf(privacyPolicy),
-            termsOfServiceText.indexOf(privacyPolicy) + privacyPolicy.length,
-            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
-        )
-        spannable.setSpan(
-            URLSpan("privacy_policy_url"),
-            termsOfServiceText.indexOf(privacyPolicy),
-            termsOfServiceText.indexOf(privacyPolicy) + privacyPolicy.length,
+            termsOfServiceText.indexOf(termsOfService),
+            termsOfServiceText.indexOf(termsOfService) + termsOfService.length,
             Spanned.SPAN_INCLUSIVE_EXCLUSIVE
         )
 
